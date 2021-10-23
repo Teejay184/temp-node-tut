5add17b6c409bc0bce094b018ccc387bd85d8e48
Teejay Swain <teejayswain@gmail.com>
2021-10-23 15:13:18 +0100

    first commit

---
+++ 2-modules.js
@@ -0,0 +1,10 @@
+// Modules
+const { teejay, john } = require('./3-names.js');
+const sayHi = require('./4-utils.js');
+const data = require('./5-alternative');
+
+require('./6-mind-grenade');
+
+//console.log(data);
+//sayHi(teejay);
+//sayHi(john);
---
+++ 3-names.js
@@ -0,0 +1,4 @@
+const teejay = 'Teejay';
+const john = 'John';
+
+module.exports = { teejay, john };
